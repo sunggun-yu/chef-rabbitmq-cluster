eb0326d4b56b1fb69a0a0fca093581661b86baf0
John Bellone <jbellone@bloomberg.net>
2015-05-14 17:06:30 -0400

    Update the license for the cookbook.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,7 +2,7 @@
 # Cookbook: rabbitmq-cluster
 # License: Apache 2.0
 #
-# Copyright (C) 2015 Sunggun Yu
+# Copyright (C) 2015 Sunggun Yu <sunggun.dev@gmail.com>
 # Copyright (C) 2015 Bloomberg Finance L.P.
 #
 
--- recipes/master.rb
+++ recipes/master.rb
@@ -2,9 +2,10 @@
 # Cookbook: rabbitmq-cluster
 # License: Apache 2.0
 #
-# Copyright (C) 2015 Sunggun Yu
+# Copyright (C) 2015 Sunggun Yu <sunggun.dev@gmail.com>
 # Copyright (C) 2015 Bloomberg Finance L.P.
 #
+
 node.default['rabbitmq-cluster']['master_node_name'] = "rabbit@#{node['hostname']}"
 include_recipe 'rabbitmq-cluster::default'
 
