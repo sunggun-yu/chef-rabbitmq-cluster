7150a04331806d5badd47894e33de6448cb435d0
John Bellone <jbellone@bloomberg.net>
2015-05-14 17:13:19 -0400

    Add the attributes default file back to project.

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook: rabbitmq-cluster
+# License: Apache 2.0
+#
+# Copyright (C) 2015 Sunggun Yu <sunggun.dev@gmail.com>
+# Copyright (C) 2015 Bloomberg Finance L.P.
+#
+
+default['rabbitmq-cluster']['master_node_name'] = 'rabbit@rabbit1'
+default['rabbitmq-cluster']['cluster_node_type'] = 'disc'
