10386055cb86329cc100ccc92090fd36ff387d8f
John Bellone <jbellone@bloomberg.net>
2015-06-15 11:51:21 -0400

    Pins the version of the RabbitMQ cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Installs and configures RabbitMQ cluster.'
 long_description 'Installs and configures RabbitMQ cluster.'
-version '1.1.0'
+version '1.1.1'
 
 depends 'poise', '~> 2.0'
-depends 'rabbitmq'
+depends 'poise-service'
+depends 'rabbitmq', '= 3.13.0'
 
 supports 'ubuntu', '>= 12.04'
 supports 'redhat', '>= 6.4'
